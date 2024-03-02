bd1a10246c72fcce033569a496f79e85f2e7da51
Daithí <davecoll3@gmail.com>
2024-03-02 20:24:01 +0000

    added templates for projects app and main.html

--- projects/views.py
+++ projects/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def projects(request):
-    return HttpResponse('Here are all of our projects')
+    return render(request, 'projects/projects.html')
 
 def project(request, pk):
-    return HttpResponse('Single project page for' + ' ' + str(pk))
+    return render(request, 'projects/single-project.html')
